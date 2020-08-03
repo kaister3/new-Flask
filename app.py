e7bc744f800a0d197a453633826a44276015c32d
Wxkwxkwxk1231 <kaister3@protonmail.com>
2020-08-03 17:56:28 +0800

    new flask

--- app.py
+++ app.py
@@ -1,21 +1,25 @@
 from flask import Flask
-from flask import escape, url_for
+from flask import escape, url_for, render_template
 app = Flask(__name__)
 
-@app.route('/user/<name>')
-def user_page(name):
-    return '%s' %name
+name = 'Grey Li'
+movies = [
+    {'title': 'My Neighbor Totoro', 'year': '1988'},
+    {'title': 'Dead Poets Society', 'year': '1989'},
+    {'title': 'A Perfect World', 'year': '1993'},
+    {'title': 'Leon', 'year': '1994'},
+    {'title': 'Mahjong', 'year': '1996'},
+    {'title': 'Swallowtail Butterfly', 'year': '1996'},
+    {'title': 'King of Comedy', 'year': '1999'},
+    {'title': 'Devils on the Doorstep', 'year': '1999'},
+    {'title': 'WALL-E', 'year': '2008'},
+    {'title': 'The Pork of Music', 'year': '2012'},
+]
 
 @app.route('/')
-def hello():
+def index():
     # 返回值作为响应的主体，默认会被浏览器作为 HTML 格式解析
-    return '<h1>Hello Totoro!</h1><img src="http://helloflask.com/totoro.gif">'
-
-@app.route('/test')
-def test_url_for():
-    print(url_for('hello'))
-    print(url_for('user_page', name='kay'))
-    print(url_for('test_url_for'))
+    return render_template('index.html', name = name, movies = movies)
 
 if __name__ == "__main__":
     app.run()
